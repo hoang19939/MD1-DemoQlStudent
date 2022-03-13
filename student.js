7ee73971c7df86d46a35dcfa4278792ad981a4fe
hoang19939 <Hoangle36humg@gmail.com>
2022-03-13 00:47:42 -0600

    DemoSever2

---
+++ student.js
@@ -0,0 +1,27 @@
+class Student {
+    constructor(id, name, age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+
+    }
+    getName(){
+        return this.name;
+    }
+    setName(newName){
+        this.Name = newName;
+    }
+    getId(){
+        return this.id;
+    }
+    setId(newId){
+        this.Id = newId;
+    }
+    getAge(){
+        return this.age;
+    }
+    setAge(newAge){
+        this.Age = newAge;
+    }
+
+}
